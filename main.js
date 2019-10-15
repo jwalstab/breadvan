cd2d47a8cff73b214a0723157a3ea51ff362bf21
Jay Walstab <jaywalstab@outlook.com>
2019-10-15 18:41:28 +0800

    firsttest

--- main.js
+++ main.js
@@ -6,7 +6,48 @@ const path = require('path');
 var fs = require('fs');
 var MongoDb = require('mongodb');
 
-var directoryPath = path.join(__dirname, 'public/imgsrv');
+var os = require('os');
+var ifaces = os.networkInterfaces();
+console.log(ifaces);
+var localAddress = "http://" + ifaces.wlan0[0].address + ":3000"
+//var localAddress = "http://" + ifaces.WLAN[0].address + ":3000"
+//var localAddress = 'http://192.168.0.135:3000'
+console.log(localAddress);
+
+//var mongoServer = "mongodb://157.245.56.30:27017"
+var mongoServer = "mongodb://127.0.0.1:27017"
+
+//SESSIONS//////
+var session = require('express-session')
+var MongoDBStore = require('connect-mongodb-session')(session);
+
+var store = new MongoDBStore({
+  uri: mongoServer,
+  databaseName: 'sessions',
+  collection: 'usersessions'
+});
+
+app.use(session({
+    secret: 'adgaigdj3wakg23o2323_3311fasa',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  }));  
+
+  var auth = function(req, res, next) {
+    if (req.session.user)
+      return next();
+    else
+      return res.redirect('/');
+  };
+
+/* 
+function AuthObj(user,name,tag) {
+    this.loggedInUser = user;
+    this.loggedInName = name;
+    this.loggedInTag = tag;
+}
+ */
 
 
 
@@ -14,8 +55,6 @@ bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser.json({limit: '50mb'}));
 
-// Parse URL-encoded bodies (as sent by HTML forms)
-//app.use(express.urlencoded());
 
 // Parse JSON bodies (as sent by API clients)
 app.use(express.json());
@@ -33,20 +72,33 @@ app.use(expressLayouts);
 /* app.listen(port, () => console.log(`Bread Van app is starting up on port ${port}!`));
  */
 var MongoClient = require('mongodb').MongoClient;
-
 var outsideDatabase;
-  MongoClient.connect("mongodb://157.245.56.30:27017", {useNewUrlParser: true, useUnifiedTopology: true}, function(err, database) {
+//157.245.56.30:27017
+//MongoClient.connect("mongodb://192.168.0.135:27017", {useNewUrlParser: true, useUnifiedTopology: true}, function(err, database) {
+  //MongoClient.connect("mongodb://127.0.0.1:27017", {useNewUrlParser: true, useUnifiedTopology: true}, function(err, database) {
+MongoClient.connect(mongoServer, {useNewUrlParser: true, useUnifiedTopology: true}, function(err, database) {
   if(err)
   throw err;
   proceduresdb = database.db('procedures');
-  patientsdb = database.db('patients');
-  proceduralistsdb = database.db('proceduralists');
+  setupdb = database.db('setup');
+  usersdb = database.db('users');
   outsideDatabase = database;
-  
+
   //db = database;
   app.listen(port);
   console.log(`Bread Van has sucessfully started on ${port} and connected to the DB ` + new Date().toLocaleDateString() + `  ` + new Date().toLocaleTimeString());
-  StartScanner();
+  ScanForExtDrives();
+    var newUser = {
+    user: "Sussex",
+    pass: "Sussex3688",
+    name: "Sussex Clinic",
+    tag: "Proceduralist"
+    }
+
+    usersdb.collection('users').insertOne(newUser, function(err,result) {
+        if(err){console.log(err)};
+        console.log("OK!");
+    });
 });
 
 app.use(function(req, res, next) {
@@ -57,30 +109,56 @@ app.use(function(req, res, next) {
   });
 
 
-var scannerDrive = path.join(__dirname, 'public/scanner');
+//FILE SCANER /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//var directoryPath = path.join(__dirname, 'public/imgsrv');
+//var hardDrivePath = 'media/medscan/HDD'
 
+//var mediaDrive = path.join(__dirname, 'public/scanner');
+var mediaDrive = '/media/medscan';
 var imgSrvDrive = path.join(__dirname, 'public/imgsrv');
 
-
-function StartScanner(){
-    fs.watch(scannerDrive, function (event, dateFolder) {
-        if (event == "change"){
-            console.log("Change detected, 5 seconds till scan starts")
-            setTimeout(function(){ ScanAction(dateFolder); }, 5000);
+var extDrives = [];
+function ScanForExtDrives(){
+    var tempExtDrives = [];
+    fs.readdir(mediaDrive, function (err,extDriveFolders){
+        if (extDriveFolders != null){
+            extDriveFolders.forEach(extDriveFolder => {
+                tempExtDrives.push(extDriveFolder);
+            });
+        }
+        //console.log(extDrives.length + "   " + tempExtDrives.length);
+        if (extDrives.length != tempExtDrives.length){
+            extDrives = tempExtDrives;
+            console.log("Changed Detected!");
+            extDrives.forEach(extDriveFolder => {
+                fs.readdir(mediaDrive + "/" + extDriveFolder + "/DCIM", function(err,dateFolders){
+                    if (err){console.log("ERROR AT DATE FOLDER LEVELS");}
+                    if (dateFolders != null){
+                        dateFolders.forEach(dateFolder => {
+                            var dateFolderWithExtDrivePath = path.join(mediaDrive, extDriveFolder, "DCIM", dateFolder);
+                            console.log(dateFolderWithExtDrivePath);
+                            ScanAction(dateFolderWithExtDrivePath,dateFolder);
+                        });
+                    }
+                });
+            });
         }
     });
+    setTimeout(function(){ScanForExtDrives();}, 1000);
 }
-
-function ScanAction(dateFolder){
-    fs.readdir(scannerDrive + "/" + dateFolder, function (err, pidFolders){
+function ScanAction(dateFolder, dateText){
+    console.log('Starting scan');
+    fs.readdir(dateFolder, function (err, pidFolders){
+        if (pidFolders == null){console.log("SCAN ERROR ON PID LOOP"); return;}
         pidFolders.forEach(pidFolder => {
-                var pid_DIR = scannerDrive + "/" + dateFolder + "/" +  pidFolder;
+                var pid_DIR = dateFolder + "/" + pidFolder;
+                console.log(pid_DIR);
                 fs.readFile(pid_DIR + "/" + 'patient.inf', 'utf8', function(err, data) {
-                if (err){console.log(err)};
-                if (data == null){console.log("NULL"); return}
+                if (data == null){console.log("no inf file @ " + pid_DIR); return}
                 var lines = data.split("\n");
+                if (lines[5] == null){console.log("cannot read inf file @ " + pid_DIR); return}
                 var examStartTime = lines[5].slice(12,14) + lines[5].slice(15,17) + lines[5].slice(18,20);
-                var procedureID = pidFolder + dateFolder + examStartTime
+                var procedureID = pidFolder + dateText + examStartTime
                 var query = {
                     procedureID: procedureID
                 }
@@ -97,7 +175,6 @@ function ScanAction(dateFolder){
         });
     });
 }
-
 function CreateProcedure(inf,procedureID,patientID, pid_DIR){
     var imgList = [];
 
@@ -109,12 +186,12 @@ function CreateProcedure(inf,procedureID,patientID, pid_DIR){
         if (docs[0] != null){console.log("procedureID " + procedureID + " already exists, not creating new procedure")}
         else{
             fs.readdir(pid_DIR, function(err,files){
+                    var imgWithID_DIR = imgSrvDrive + '/' + procedureID;
+                    fs.mkdir(imgWithID_DIR,(err) => {if(err){console.log(err)};});
                     for (let index = 0; index < files.length; index++) {
                         const img = files[index];
-                        var imgWithID_DIR = imgSrvDrive + '/' + procedureID;
-                        fs.mkdir(imgWithID_DIR,(err) => {});
                         fs.copyFile(pid_DIR + '/' + img, imgWithID_DIR + '/' + 'IMG' + index + '.JPG', (err) => {
-                            if (err) throw err;
+                            if (err) {console.log(err)};
                         });
                         imgList.push('IMG' + index + '.JPG');
                     }
@@ -128,16 +205,19 @@ function CreateProcedure(inf,procedureID,patientID, pid_DIR){
                     procedureDate: inf[4].slice(11,23) + " " + inf[5].slice(12,17),
                     patientName: inf[8].slice(3, -1),
                     patientDOB: inf[9].slice(6,-1),
-                    patientAge: parseInt(inf[10].slice(6,-1)),
+                    patientAge: inf[10].slice(6,-1),
                     patientSex: inf[11].slice(7,-1),
                     clinic: inf[12].slice(3, -1),
                     proceduralist: inf[13].slice(4,-1),
                     reason: '',
                     findings: '',
+                    bowelPreparation: '',
+                    polyps: '',
                     completeColon: '',
                     haemorrhoids: '',
                     followUpRadio: '',
                     followUpDetails: '',
+                    followUpDiscuss: '',
                     followUpDate: '',
                     imgs:imgList,
                     diagramImg: null,
@@ -150,25 +230,31 @@ function CreateProcedure(inf,procedureID,patientID, pid_DIR){
     });
 }
 
-
-
-
-
-
-
-
-
   
-//WEB SERVER START //////////////////////////////
-app.get('/', (req, res) => res.render('home'));
+//WEB SERVER START //////////////////////////////////////////////////////////////////////////////////////////
 
-app.get('/newprocedure', (req, res) => {
-    res.render('newprocedure');
+app.get('/home', auth, function (req, res) {
+    res.render('home',{
+        loggedInName:req.session.name,
+        loggedInTag:req.session.tag,
+        localAddress:localAddress
+    });
 });
-app.get('/viewprocedure', (req, res) => {
-    res.render('viewprocedure');
+
+app.get("/", function(req, res) {
+      if (req.session.user){
+        res.redirect('home');
+      }
+      else{
+        res.render('sign_in', { 
+            layout: 'emptylayout',
+            status: "",
+            localAddress:localAddress 
+        });
+      }
 });
-app.get('/openprocedure/:procedureID', (req, res) => {
+
+app.get('/editprocedure/:procedureID', (req, res) => {
     //var theId = MongoDb.ObjectId(req.params.procedureid);
     var openProcedureQuery = {
         procedureID: req.params.procedureID
@@ -176,48 +262,115 @@ app.get('/openprocedure/:procedureID', (req, res) => {
     console.log(openProcedureQuery)
     proceduresdb.collection('procedures').find(openProcedureQuery).toArray (function(err,docs) {
         console.log(docs);
-        res.render('openprocedure',{
+        res.render('editprocedure',{
+            loggedInName:req.session.name,
+            loggedInTag:req.session.tag,
+            localAddress: localAddress,
             data: docs[0]
         });
     });
 });
-app.get('/viewprocedures', (req, res) => {
-    res.render('viewprocedures');
-});
-app.get('/gallery', (req, res) => {
-    res.render('gallery');
-});
-app.get('/canvas', (req, res) => {
-    res.render('canvas');
-});
 
-//API SERVER START //////////////////////////////
-app.post('/submitnewprocedure', (req, res) => {
-    var base64Data = req.body.imgdata.replace(/^data:image\/png;base64,/, "");
-    delete req.body.imgdata;
-    proceduresdb.collection('procedures').insertOne(req.body, function(err,result) {
-        console.log();
-        require("fs").writeFile( directoryPath + "/"+ result.ops[0]._id + ".png", base64Data, 'base64', function(err) {
-        if(err){console.log(err)};
+app.get('/printprocedure/:procedureID', (req, res) => {
+    //var theId = MongoDb.ObjectId(req.params.procedureid);
+    var openProcedureQuery = {
+        procedureID: req.params.procedureID
+    }
+    console.log(openProcedureQuery)
+    proceduresdb.collection('procedures').find(openProcedureQuery).toArray (function(err,docs) {
+        console.log(docs);
+        res.render('printprocedure',{
+            loggedInName:req.session.name,
+            loggedInTag:req.session.tag,
+            localAddress: localAddress,
+            data: docs[0]
         });
     });
-    res.render('newprocedure');
 });
 
+app.get('/viewprocedures', (req, res) => {
+    res.render('viewprocedures',{
+        loggedInName:req.session.name,
+        loggedInTag:req.session.tag,
+        localAddress:localAddress
+    });
+});
+
+app.get('/setup', (req, res) => {
+    res.render('setup',{
+        loggedInName:req.session.name,
+        loggedInTag:req.session.tag,
+        localAddress:localAddress
+    });
+});
+
+
+
+
+//API SERVER START //////////////////////////////////////////////////////////////////////////////////////
+
+//AUTHENTICATION
+
+
+
+app.post('/logincheck', function (req, res) {
+    usersdb.collection('users').find({}).toArray(function(err, docs){
+      loggedIn = false;
+      docs.forEach(element => {
+        if (element.user === req.body.user){
+          if (element.pass === req.body.pass){
+            req.session.user = element.user;
+            req.session.name = element.name;
+            req.session.tag = element.tag;
+            loggedIn = true;
+          }
+        }
+      });
+      if (loggedIn == true){
+        res.redirect('home');
+      }
+      else{
+        res.render('sign_in', { 
+            layout: 'emptylayout',
+            status: "Incorrect username or password.",
+            localAddress:localAddress 
+        });
+      }
+    });
+});
+
+app.get("/sign_out", function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
+  });
+
+
+//PROCEDURES
+
 app.post('/updateprocedure', (req,res) =>{
-    console.log(req.body);
+    req.body.new = false;
     var updateProcedureQuery = {
         procedureID: req.body.procedureID
     }
+    if (req.body.bowelPreparation == null){ req.body.bowelPreparation = "No"}
+    if (req.body.polyps == null){ req.body.polyps = "No";}
+    if (req.body.completeColon == null){ req.body.completeColon = "No"}
+    if (req.body.haemorrhoids == null){ req.body.haemorrhoids = "No"}
+    if (req.body.diverticularDisease == null){ req.body.diverticularDisease = "No"}
+    
     proceduresdb.collection('procedures').deleteOne(updateProcedureQuery).then (function(result) {
-        console.log(result);
+        if(result.deletedCount == 1){console.log("Record " + req.body.procedureID + " cleared")}
+        else{console.log("Record: " + req.body.procedureID + " not cleared")}
         proceduresdb.collection('procedures').insertOne(req.body, function(err,result) {
-            console.log('record updated :' + req.body.procedureID);
-            res.end();
+            var base64Data = req.body.imgdata.replace(/^data:image\/png;base64,/, "");
+            require("fs").writeFile( imgSrvDrive + "/"+ req.body.procedureID + "/" + "diagram.png", base64Data, 'base64', function(err) {
+                if(err){console.log(err)};
+                console.log('Record:' + req.body.procedureID + ' recreated.');
+                res.redirect('viewprocedures');
+            });
         });
     });
 })
-
 app.get('/getprocedures', (req, res) => {
     proceduresdb.collection('procedures').find({}).toArray (function(err,docs) {
         res.send(docs);
@@ -226,51 +379,61 @@ app.get('/getprocedures', (req, res) => {
 app.post('/deleteprocedure', (req, res) => {
     proceduresdb.collection('procedures').deleteOne(req.body).then (function(err,docs) {
         var response = {
-            msg: "Delete request for report ID " + req.body.procedureID + " recieved"
+            msg: "Delete request for report ID " + req.body.procedureID + " recieved "
         }
+        var procedureImageFolder = imgSrvDrive + "/" + req.body.procedureID;
+        var deleteProcedureImageFilesCount = 0;
+        fs.readdir(procedureImageFolder, function (err, files){
+            if (files == null){console.log('delete folders returned nothing');return;}
+            files.forEach(file => {
+                deleteProcedureImageFilesCount++;
+                var pathOfFileToDelete = imgSrvDrive + "/" + req.body.procedureID + "/" + file;
+                fs.unlinkSync(pathOfFileToDelete);
+                if (deleteProcedureImageFilesCount == files.length){
+                    fs.rmdir(procedureImageFolder, (err) =>{
+                        if (err){console.log(err);}
+                    });
+                }
+            });
+        });
         res.send(response);
     });
 });
 
-
+//IMAGES
 app.get('/imgsrv/:imagename', (req, res) => {
     res.sendFile(__dirname + '/public/imgsrv/' + req.params.imagename);
 });
 
 
+//SETUP
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* app.post('/sendimagedata', (req,res) =>{
-    console.log(req.body.data);
-    var response = {
-        msg: "OK"
-    }
-    //var base64Data = req.body.data;
-    res.send(response);
-}); */
-
-/* fs.readdir(directoryPath, function (err, files) {
-    //handling error
-    if (err) {
-        return console.log('Unable to scan directory: ' + err);
-    } 
-    //listing all files using forEach
-    files.forEach(function (file) {
-        // Do whatever you want to do with the file
-        console.log(file); 
+app.post('/setupnew/:collection', (req, res) => {
+    console.log(req.body);
+    setupdb.collection(req.params.collection).insertOne(req.body).then (function() {
+        console.log('ok')
     });
-}); */
+    res.render('setup',{
+        localAddress:localAddress
+    });
+});
+
+app.get('/setupget/:collection', (req, res) => {
+    setupdb.collection(req.params.collection).find({}).toArray (function(err,docs) {
+        res.send(docs);
+    });
+});
+
+app.post('/setupdelete/:collection', (req, res) => {
+    console.log(req.body);
+    setupdb.collection(req.params.collection).deleteOne(req.body).then (function(err,docs) {
+/*         var response = {
+            msg: "Delete request for report ID " + req.body._id + " recieved"
+        } */
+        res.render('setup',{
+            localAddress:localAddress
+        });
+    });
+});
+
+
